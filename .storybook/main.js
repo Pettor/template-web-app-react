0cc8eef930c09bb008d0455505d6895720636875
Petter Hancock <pettor@gmail.com>
2022-11-04 12:07:04 +0100

    Remove unsupported React-Intl plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,12 +5,16 @@ const { default: tsconfigPaths } = require("vite-tsconfig-paths");
 module.exports = {
   framework: "@storybook/react-vite",
   stories: ["../src/**/*.stories.@(ts|tsx)"],
-  addons: ['@storybook/addon-essentials']
+  addons: ["@storybook/addon-essentials", "@storybook/addon-a11y", "storybook-dark-mode"],
 
   async viteFinal(config) {
     return mergeConfig(config, {
       base: "./",
       plugins: [tsconfigPaths()],
+      // Add storybook-specific dependencies to pre-optimization
+      optimizeDeps: {
+        include: ["@storybook/addon-a11y", "storybook-dark-mode"],
+      },
     });
   },
 };
