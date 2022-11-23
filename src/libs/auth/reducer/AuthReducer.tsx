323eac65ea1f0bc829c0a7ae50bee6b36b38b96f
Petter Hancock <pettor@gmail.com>
2022-11-23 20:57:16 +0100

    Fix issue where refresh-token is sent twice in React 18

--- src/libs/auth/reducer/AuthReducer.tsx
+++ src/libs/auth/reducer/AuthReducer.tsx
@@ -1,4 +1,6 @@
+import { LocalStorage } from "../../storage/local-storage/LocalStorage";
 import { AuthData } from "../types/AuthData";
+import { RefreshToken } from "../types/RefreshToken";
 
 type AuthActions = { type: "auth/loading" } | { type: "auth/login"; payload: AuthData } | { type: "auth/logout" };
 
@@ -7,14 +9,20 @@ interface AuthState {
   data: AuthData | null;
 }
 
+const refreshTokenStorage = new LocalStorage<RefreshToken | null>("refresh-token");
+
 const AuthReducer = (prev: AuthState, action: AuthActions): AuthState => {
   switch (action.type) {
     case "auth/loading": {
       return { ...prev, status: "idle" };
     }
-    case "auth/login":
+    case "auth/login": {
+      const { refreshToken } = action.payload;
+      refreshTokenStorage.set(refreshToken);
       return { ...prev, data: action.payload, status: "authenticated" };
+    }
     case "auth/logout":
+      refreshTokenStorage.set(null);
       return { ...prev, data: null, status: "authenticating" };
     default:
       return prev;
