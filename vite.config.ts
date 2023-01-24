9d543d962d65a1de0b58754ddc6198434cfe9bd0
Petter Hancock <pettor@gmail.com>
2023-01-24 10:35:11 +0100

    Add mkcert and https proxy to allow http2 during development

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,35 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig, loadEnv } from "vite";
+import { UserConfig, defineConfig, loadEnv } from "vite";
+import proxy from "vite-plugin-http2-proxy";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
   const envs = loadEnv(mode, process.cwd());
   process.env = { ...process.env, ...envs };
 
-  const plugins = {
+  const commonConfig = {
     plugins: [react()],
   };
 
-  let overrides = {};
-  switch (command) {
-    case "serve":
-      overrides = {
-        server: {
-          proxy: {
-            "/api": {
-              target: `http://${process.env.VITE_CONNECT_HOST}:${process.env.VITE_CONNECT_PORT}`,
-              changeOrigin: true,
-              secure: false,
-            },
+  if (command === "serve") {
+    return {
+      ...commonConfig,
+      plugins: [
+        ...commonConfig.plugins,
+        mkcert(),
+        proxy({
+          "^/api": {
+            target: `http://${process.env.VITE_CONNECT_HOST}:${process.env.VITE_CONNECT_PORT}`,
+            secure: false,
           },
-        },
-      };
-      break;
-    default:
-    case "build":
-      overrides = {};
-      break;
+        }),
+      ],
+      server: {
+        https: true,
+      },
+    } as UserConfig;
   }
 
-  return { ...plugins, ...overrides };
+  return commonConfig;
 });
