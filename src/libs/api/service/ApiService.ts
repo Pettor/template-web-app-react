3cd224cc7ef43ea7d58d91cdec6fa5009ca3ba0c
Petter Hancock <pettor@gmail.com>
2023-01-30 08:32:28 +0100

    Fix for Axios header types

--- src/libs/api/service/ApiService.ts
+++ src/libs/api/service/ApiService.ts
@@ -1,1 +1,38 @@
+import { AxiosInstance, AxiosResponse, RawAxiosRequestConfig, RawAxiosRequestHeaders } from "axios";
+import { JwtToken } from "../../auth/types/JwtToken";
+import { TokenRequestRequest } from "./requests/TokenRequestRequest";
+import { TokenDto } from "./responses/TokenDto";
 
+const defaultHeaders: RawAxiosRequestHeaders = {
+  tenant: "root",
+};
+
+const defaultConfig: RawAxiosRequestConfig = {
+  headers: defaultHeaders,
+};
+
+export async function tokenRequest(client: AxiosInstance, data: TokenRequestRequest): Promise<AxiosResponse<TokenDto>> {
+  return await client.post<TokenDto>("/api/tokens", data, defaultConfig);
+}
+
+export async function tokenRefresh(client: AxiosInstance): Promise<AxiosResponse<TokenDto>> {
+  return await client.get<TokenDto>("/api/tokens/refresh", defaultConfig);
+}
+
+export async function createPostRequest(client: AxiosInstance, url: string, token: JwtToken, data: unknown) {
+  const config: RawAxiosRequestConfig = {
+    ...defaultConfig,
+    headers: { ...defaultHeaders, Authorization: `Bearer ${token}` },
+  };
+
+  return await client.post(url, data, config);
+}
+
+export async function createGetRequest(client: AxiosInstance, url: string, token: JwtToken) {
+  const config: RawAxiosRequestConfig = {
+    ...defaultConfig,
+    headers: { ...defaultHeaders, Authorization: `Bearer ${token}` },
+  };
+
+  return await client.get(url, config);
+}
