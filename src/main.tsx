90814c93f84d790222d1840d58da6b0d1dc14d78
Petter Hancock <pettor@gmail.com>
2023-02-26 20:02:47 +0100

    Add ts-reset to make typescript a bit more ergonomic

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+// Used to make typescript a bit more ergonomic
+import "@total-typescript/ts-reset";
 
 const container = document.getElementById("root");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
