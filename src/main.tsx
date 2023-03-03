c3a2ee598bb97cdda9d09efab87e3e355431edf9
Petter Hancock <pettor@gmail.com>
2023-03-03 21:53:59 +0100

    update location for ts-reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
-// Used to make typescript a bit more ergonomic
-import "@total-typescript/ts-reset";
 
 const container = document.getElementById("root");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
