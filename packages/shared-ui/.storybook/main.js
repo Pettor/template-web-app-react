3a74d9b91822bb3c64041a55467d882c6efae669
Petter Hancock <pettor@gmail.com>
2023-07-02 22:49:31 +0200

    feat: add storybook/interactions addon

--- packages/shared-ui/.storybook/main.js
+++ packages/shared-ui/.storybook/main.js
@@ -1,55 +1,40 @@
-import type { AddonOptionsVite } from "@storybook/addon-coverage";
-import type { StorybookConfig } from "@storybook/react-vite";
-import type { UserConfig } from "vite";
+/* eslint-disable */
 import { mergeConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const config: StorybookConfig = {
+const config = {
   framework: "@storybook/react-vite",
   stories: ["../src/**/*.stories.@(ts|tsx)"],
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
   addons: [
     "@storybook/addon-a11y",
-    {
-      name: "@storybook/addon-coverage",
-      options: {
-        istanbul: {
-          exclude: ["**/storybook/**"],
-        },
-      } satisfies AddonOptionsVite,
-    },
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-styling",
     "@storybook/addon-viewport",
   ],
   docs: {
-    defaultName: "Docs",
-  },
-  refs: {
-    "shared-ui": {
-      title: "Common Components",
-      url: process.env.NODE_ENV === "development" ? "http://localhost:6006/" : "shared-ui/",
-    },
+    defaultName: 'Docs',
   },
   async viteFinal(config) {
     return mergeConfig(config, {
       base: "./",
-      plugins: [tsconfigPaths()],
+      plugins: [
+        tsconfigPaths()
+      ],
       // Add storybook-specific dependencies to pre-optimization
       optimizeDeps: {
         include: [
           "@storybook/addon-a11y",
-          "@storybook/addon-coverage",
           "@storybook/addon-essentials",
           "@storybook/addon-interactions",
           "@storybook/addon-styling",
           "@storybook/addon-viewport",
         ],
       },
-    } satisfies UserConfig);
+    });
   },
 };
 
