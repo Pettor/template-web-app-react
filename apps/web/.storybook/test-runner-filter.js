790ba957d85e8ce6d1ec55359d496958fe68d8be
Petter Hancock <pettor@gmail.com>
2023-07-15 10:09:49 +0200

    chore: filter stories for unit testing

---
+++ apps/web/.storybook/test-runner-filter.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const FILTER_STORIES = [`${path.sep}storybook${path.sep}`]
+
+module.exports = testPaths => {
+  const allowedPaths = testPaths
+    .filter(fileName => {
+      for (const filter of FILTER_STORIES) {
+        if (fileName.includes(filter)) {
+          return false;
+        }
+      }
+      
+      return true;
+    })
+    .map(test => ({ test }));
+
+  return {
+    filtered: allowedPaths,
+  };
+};
