cb70303406544d269c0fbfdd46dd9d355535c5eb
Petter Hancock <pettor@gmail.com>
2024-02-08 20:39:28 +0100

    feat: use standalone vite config for storybook

--- packages/shared-ui/.storybook/main.ts
+++ packages/shared-ui/.storybook/main.ts
@@ -1,14 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import type { UserConfig } from "vite";
-import { mergeConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-  framework: "@storybook/react-vite",
-  stories: ["../src/**/*.stories.@(ts|tsx)"],
-  features: {
-    storyStoreV7: true,
-  },
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-coverage",
@@ -17,34 +9,22 @@ const config: StorybookConfig = {
     "@storybook/addon-styling",
     "@storybook/addon-viewport",
   ],
+  core: {
+    builder: {
+      name: "@storybook/builder-vite",
+      options: {
+        viteConfigPath: ".storybook/vite.config.ts",
+      },
+    },
+  },
   docs: {
     defaultName: "Docs",
   },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      base: "./",
-      plugins: [tsconfigPaths()],
-      // Add storybook-specific dependencies to pre-optimization
-      optimizeDeps: {
-        include: [
-          "@storybook/addon-a11y",
-          "@storybook/addon-interactions",
-          "@storybook/addon-styling",
-          "@storybook/addon-viewport",
-        ],
-      },
-      build: {
-        rollupOptions: {
-          onwarn: (warning, warn) => {
-            if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-              return;
-            }
-            warn(warning);
-          },
-        },
-      },
-    } satisfies UserConfig);
+  framework: "@storybook/react-vite",
+  features: {
+    storyStoreV7: true,
   },
+  stories: ["../src/**/*.stories.@(ts|tsx)"],
 };
 
 export default config;
