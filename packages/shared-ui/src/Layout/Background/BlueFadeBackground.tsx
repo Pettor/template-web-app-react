d52eadd9765c3fb648847cd28dcd49b47bf8a791
Petter Hancock <pettor@gmail.com>
2024-03-18 08:39:08 +0100

    feat: trim z-index for layouts

--- packages/shared-ui/src/Layout/Background/BlueFadeBackground.tsx
+++ packages/shared-ui/src/Layout/Background/BlueFadeBackground.tsx
@@ -3,7 +3,7 @@ import type { ReactElement } from "react";
 export function BlueFadeBackground(): ReactElement {
   return (
     <div
-      className="absolute top-0 z-[-1] h-full w-full bg-base-100
+      className="absolute top-0 z-[-2] h-full w-full bg-base-100
         bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
     />
   );
