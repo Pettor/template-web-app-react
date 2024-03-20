c0179ce00643dad8ca39ab6d3f267217ce402eb3
Petter Hancock <pettor@gmail.com>
2024-03-20 12:10:37 +0100

    fix: vite preload error

--- apps/web/src/core/App.tsx
+++ apps/web/src/core/App.tsx
@@ -1,9 +1,22 @@
-import { type ReactElement } from "react";
+import { type ReactElement, useEffect } from "react";
 import { AppProviders } from "./AppProviders";
 import { AppTheme } from "./AppTheme";
 import { AppRoutes } from "./routes/AppRoutes";
 
 export function App(): ReactElement {
+  function handlePreloadError(): void {
+    // Used if Vite has issue loading chunks
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    window.addEventListener("vite:preloadError", handlePreloadError);
+
+    return () => {
+      window.removeEventListener("vite:preloadError", handlePreloadError);
+    };
+  }, []);
+
   return (
     <AppProviders>
       <AppTheme>
