b3479fb794ddd1b0eb2990c15a1f262361c04920
Petter Hancock <pettor@gmail.com>
2025-02-26 21:15:28 +0100

    feat: migrate eslint from version 8 to 9

--- configs/eslint-config-custom/base.js
+++ configs/eslint-config-custom/base.js
@@ -1,91 +1,86 @@
-module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended"
-  ],
-  "ignorePatterns": ["**/dist/*", "**/node_modules/*"],
-  "plugins": ["react", "react-hooks", "@typescript-eslint", "prettier", "formatjs", "import"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+import js from "eslint-plugin-js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
+import formatjs from "eslint-plugin-formatjs";
+import eslintImport from "eslint-plugin-import";
+import storybook from "eslint-plugin-storybook";
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsparser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
     },
-    "ecmaVersion": 12,
-    "sourceType": "module"
-  },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  },
-  "rules": {
-    // Common
-    "import/order": [
-      "error",
-      {
-        "alphabetize": {
-          "order": "asc",
-          "caseInsensitive": true
+    linterOptions: {
+      ignorePatterns: ["**/dist/*", "**/node_modules/*"],
+    },
+    plugins: {
+      js,
+      "@typescript-eslint": tseslint,
+      react,
+      "react-hooks": reactHooks,
+      prettier,
+      formatjs,
+      import: eslintImport,
+      storybook,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          alphabetize: { order: "asc", caseInsensitive: true },
+          groups: ["builtin", "external", "parent", "sibling", "index"],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
         },
-        "groups": ["builtin", "external", "parent", "sibling", "index"],
-        "pathGroups": [
-          {
-            "pattern": "react",
-            "group": "external",
-            "position": "before"
-          }
-        ],
-        "pathGroupsExcludedImportTypes": ["builtin"]
-      }
-    ],
-    "func-style": ["error", "declaration", { "allowArrowFunctions": false }],
-    // React
-    "react/react-in-jsx-scope": "off",
-    "react/function-component-definition": [
-      "warn",
-      {
-        "namedComponents": ["function-declaration"]
-      }
-    ],
-    "react-hooks/exhaustive-deps": "error",
-    // Typescript
-    "@typescript-eslint/no-empty-interface": [
-      "error",
-      {
-        "allowSingleExtends": true
-      }
-    ],
-    "@typescript-eslint/explicit-function-return-type": "error",
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      {
-        "prefer": "type-imports"
-      }
-    ],
-    // Plugins
-    "prettier/prettier": "error",
-    "formatjs/enforce-id": [
-      "error",
-      {
-        "idInterpolationPattern": "[sha512:contenthash:base64:6]"
-      }
-    ]
+      ],
+      "func-style": ["error", "declaration", { allowArrowFunctions: false }],
+      "react/react-in-jsx-scope": "off",
+      "react/function-component-definition": [
+        "warn",
+        {
+          namedComponents: ["function-declaration"],
+        },
+      ],
+      "react-hooks/exhaustive-deps": "error",
+      "@typescript-eslint/no-empty-interface": [
+        "error",
+        { allowSingleExtends: true },
+      ],
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "prettier/prettier": "error",
+      "formatjs/enforce-id": [
+        "error",
+        { idInterpolationPattern: "[sha512:contenthash:base64:6]" },
+      ],
+    },
   },
-  "overrides": [
-    {
-      // Disable for Storybook
-      "files": ["*stories.tsx", "main.ts"],
-      "rules": {
-        "@typescript-eslint/explicit-function-return-type": "off"
-      }
-    }
-  ]
-};
+  {
+    files: ["*stories.tsx", "main.ts"],
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  },
+];
