83ad4b09359ce40151f80fb9e0e884784e8d7399
Petter Hancock <pettor@gmail.com>
2025-03-15 19:43:11 +0100

    chore(auth): restructure types

--- apps/web/src/core/AppProviders.tsx
+++ apps/web/src/core/AppProviders.tsx
@@ -2,8 +2,8 @@ import type { ReactElement } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { validateCrypto } from "react-package";
 import { AppLocales } from "./AppLocales";
-import { AuthProvider } from "./auth/AuthContext";
 import { PwaProviderModule } from "~/components/modules/pwa-provider-module/PwaProviderModule";
+import { AuthProvider } from "./auth/AuthProvider";
 
 const queryClient = new QueryClient();
 
--- apps/web/src/core/auth/AuthContext.tsx
+++ apps/web/src/core/auth/AuthContext.tsx
@@ -1,52 +1,11 @@
-import type { Dispatch, ReactElement, ReactNode } from "react";
-import { createContext, useReducer } from "react";
-import { usePostRefreshTokenMutate } from "api-package";
-import { useRunOnce } from "react-package";
-import type { AuthActions, AuthState } from "./AuthReducer";
-import { AuthReducer } from "./AuthReducer";
+import type { Dispatch } from "react";
+import { createContext } from "react";
+import type { AuthActions } from "./AuthReducer";
+import type { AuthState } from "~/classes/auth/AuthState";
 
-interface IAuthContext {
+export interface IAuthContext {
   state: AuthState;
   dispatch: Dispatch<AuthActions>;
 }
 
-const AuthContext = createContext<IAuthContext>({} as IAuthContext);
-
-const initialState: AuthState = {
-  status: "idle",
-};
-
-interface Props {
-  children: ReactNode;
-}
-
-function AuthProvider({ children }: Props): ReactElement {
-  const [state, dispatch] = useReducer(AuthReducer, initialState);
-  const { mutateAsync: refreshToken } = usePostRefreshTokenMutate();
-
-  useRunOnce({
-    func: async () => {
-      try {
-        // Run once on app load to check if the user is logged in
-        await refreshToken();
-        dispatch({ type: "auth/login" });
-      } catch {
-        dispatch({ type: "auth/logout" });
-      }
-    },
-  });
-
-  return (
-    <AuthContext.Provider
-      value={{
-        state,
-        dispatch,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-}
-
-export type { IAuthContext };
-export { AuthProvider, AuthContext };
+export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
