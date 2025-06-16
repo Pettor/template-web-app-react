2f89c34d09076c35609f9e2ff36b2be329217bb4
Petter Hancock <pettor@gmail.com>
2025-06-16 20:53:54 +0200

    feat(storybook): upgrade to storybook 9

--- apps/web/src/components/forms/login/LoginForm.stories.tsx
+++ apps/web/src/components/forms/login/LoginForm.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { userEvent, within, expect } from "@storybook/test";
+import { userEvent, within, expect } from "storybook/test";
 import { ContainerDecorator } from "storybook-package";
 import { LoginForm as Component } from "./LoginForm";
 import type { LoginFormProps as Props } from "./LoginForm";
